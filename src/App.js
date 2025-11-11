7651da0226e2eac325dd112caea114658f1eda65
975569220@qq.com <975569220@qq.com>
2025-11-12 00:18:16 +0800

    add controled component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -5,11 +7,12 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState(initialItems);
   return (
     <div className="App">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form setItems={setItems} items={items} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,20 +22,57 @@ function Logo() {
   return <h1>🏝️Far Away🧳</h1>;
 }
 
-function Form() {
+function Form(props) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    // Form submission logic here
+    if (!description) return;
+    setDescription("");
+    setQuantity(1);
+    props.setItems([
+      ...props.items,
+      {
+        id: props.items.length + 1,
+        description: description,
+        quantity: quantity,
+        packed: false,
+      },
+    ]);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+    </form>
   );
 }
 
-function PackingList() {
+function PackingList(props) {
+  console.log(props);
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item}></Item>
+        {props.items.map((item) => (
+          <Item item={item} key={item.id}></Item>
         ))}
       </ul>
     </div>
